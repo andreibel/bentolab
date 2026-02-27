ff93e7e22e178b502ce657390b4e6644c33b3621
andreibel <andreinw12@gmail.com>
2026-02-28 00:23:37 +0200

    feat(org-service): add createOrganization method for organization creation with user ID validation in controller

---
+++ backend/org-service/src/main/java/io/bento/orgservice/controller/OrgController.java
@@ -0,0 +1,37 @@
+package io.bento.orgservice.controller;
+
+import io.bento.orgservice.dto.request.CreateOrgRequest;
+import io.bento.orgservice.dto.request.TransferOrgOwnershipRequest;
+import io.bento.orgservice.dto.request.UpdateOrgRequest;
+import io.bento.orgservice.dto.request.UpdateOrgSettingsRequest;
+import io.bento.orgservice.dto.response.OrgResponse;
+import io.bento.orgservice.service.OrgService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/orgs")
+@RequiredArgsConstructor
+public class OrgController {
+
+
+    private final OrgService orgService;
+
+    @PostMapping
+    public ResponseEntity<OrgResponse> createOrganization(
+            @RequestHeader("X-User-Id") UUID userid,
+            @Valid @RequestBody CreateOrgRequest request
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(orgService.createOrg(userid, request));
+    }
+
+
+}
+
